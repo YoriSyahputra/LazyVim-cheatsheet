7e5df2c9e5f81c6a298931656499ba25b87211c7
YoriSyahputra <yorisyahputra24@gmail.com>
2026-09-21 01:33:55 +0700

    fix: remove appendSwitch from main.js and use CLI wrapper for sandbox bypass

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
-app.comandLine.appendSwitch('no-sandbox');
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
